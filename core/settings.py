950dbe610c790801da12607ccad281a4b2c2e20a
IDOHOU Sande Augustin <asidev7@gmail.com>
2024-08-11 23:36:15 +0100

    UPDATE

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://django-django-code.heqdgl.easypanel.host/']
 
 
 # Application definition
